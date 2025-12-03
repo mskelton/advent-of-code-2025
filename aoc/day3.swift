2ab475a322dd562008236188ea333ec4190b080b
Mark Skelton <mdskelton99@gmail.com>
2025-12-02 23:18:24 -0600

    Day 3 part 1

--- aoc/day3.swift
+++ aoc/day3.swift
@@ -7,12 +7,40 @@ func Day3(_ type: InputType) {
     let content = readFile(type == .simple ? "~/dev/aoc/day3-simple.txt" : "~/dev/aoc/day3.txt")
     let lines = content.components(separatedBy: .newlines)
 
-    part1(lines)
+    print("Part 1: \(part1(lines))")
     part2(lines)
 }
 
-private func part1(_ lines: [String]) {
+private func part1(_ lines: [String]) -> Int {
+    var total = 0
 
+    for line in lines {
+        let joltage = parseJoltage(line)
+        total += joltage
+    }
+
+    return total
+}
+
+private func parseJoltage(_ bank: String) -> Int {
+    var joltage: (Int, Int) = (0, 0)
+
+    for char in bank {
+        let digit = Int(String(char)) ?? 0
+
+        // Shift the right to the left if it creates a higher number
+        if joltage.1 > joltage.0 {
+            joltage.0 = joltage.1
+            joltage.1 = 0
+        }
+
+        // Push new digits in from the right
+        if digit > joltage.1 {
+            joltage.1 = digit
+        }
+    }
+
+    return joltage.0 * 10 + joltage.1
 }
 
 private func part2(_ lines: [String]) {
--- aoc/main.swift
+++ aoc/main.swift
@@ -1,9 +1,10 @@
 import Foundation
 
-let type: InputType = .full
+let type: InputType = .simple
 let days = [
     Day1,
     Day2,
+    Day3,
 ]
 
 days.last?(type)
